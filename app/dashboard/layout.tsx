397f7c0ea52086fce9df6cd702f762695500ffb4
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-06 21:34:50 +0600

    Added Header and Footer to layout of dashboard

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+"use client";
 
-function layout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex flex-col min-h-screen border-s-gray-50">
+      <Header />
+      <main className="flex-1 px-4 py-6">{children}</main>
+      <Footer />
+    </div>
+  );
 }
-
-export default layout;
