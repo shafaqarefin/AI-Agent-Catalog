7609a540fed823d124638ae4b5598ec2184e8bfb
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-06 21:36:26 +0600

    dashboard fetching logic from mock agent server and passing to agentgrid component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import AgentGrid from "@/components/agent/AgentsGrid";
+import CatalogHeader from "@/components/agent/Header";
 
-function Dashboard() {
-  return <div>Welcome to Dashboard</div>;
+export default async function Dashboard() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/agents`, {
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch agents");
+  }
+  const agents = await res.json();
+
+  return (
+    <>
+      <h1>Welcome to Dashboard</h1>
+      <CatalogHeader />
+      <AgentGrid agents={agents} />
+    </>
+  );
 }
-
-export default Dashboard;
