7e452d9ced9a6a0d53519c7bbb84f6c325795bc0
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-07 13:30:12 +0600

    Added SEO support,Added route protection,added SSR support

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,20 +1,39 @@
-import AgentGrid from "@/components/agent/AgentsGrid";
 import CatalogHeader from "@/components/agent/Header";
+import HydrationAgent from "@/components/agent/HydrationAgent"; // new
+import AgentGrid from "@/components/agent/AgentsGrid";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+export const metadata = {
+  title: "AI Agent Dashboard",
+  description:
+    "Discover and manage AI agents filtered by category, pricing, and status.",
+  keywords: ["AI Agents", "Dashboard", "Filters", "AI Catalog"],
+  openGraph: {
+    title: "AI Agent Dashboard",
+    description: "Explore AI agents with advanced filters and search.",
+    url: `https://${process.env.NEXT_PUBLIC_BASE_URL}.com/dashboard`,
+    siteName: "AI Agent Catalog",
+    type: "website",
+  },
+  robots: "index, follow",
+};
 
-export default async function Dashboard() {
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/");
+  }
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/agents`, {
     cache: "no-store",
   });
-  if (!res.ok) {
-    throw new Error("Failed to fetch agents");
-  }
   const agents = await res.json();
 
   return (
     <>
-      <h1>Welcome to Dashboard</h1>
       <CatalogHeader />
-      <AgentGrid agents={agents} />
+      <HydrationAgent agents={agents} />
+      <AgentGrid />
     </>
   );
 }
