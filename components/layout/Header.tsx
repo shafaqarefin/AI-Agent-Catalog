5fdc67a0cfc3ae118fff162f5bf4b67bd23bd6e8
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-07 16:41:38 +0600

    made header responsive

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,9 +2,14 @@ import UserInfo from "../user/UserInfo";
 
 export default function Header() {
   return (
-    <header className="w-full border-b px-6 py-4 flex justify-between items-center">
-      <h1 className="text-2xl font-semibold">ArkLab AI Agent Catalog</h1>
-      <UserInfo />
+    <header className="w-full border-b px-4 py-3 flex justify-between items-center">
+      <h1 className="hidden sm:block text-xl font-semibold">
+        ArkLab AI Agent Catalog
+      </h1>
+
+      <div className="text-xs sm:text-sm">
+        <UserInfo />
+      </div>
     </header>
   );
 }
