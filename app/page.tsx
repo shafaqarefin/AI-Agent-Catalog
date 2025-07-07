7b1f6e705ef0b6b177e8df13e9998a3736ebf4e3
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-07 16:34:48 +0600

    Updated Login UI

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,11 @@ import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
 
 export default function Login() {
   return (
@@ -19,35 +16,10 @@ export default function Login() {
       <Card className="w-full max-w-sm ">
         <CardHeader>
           <CardTitle>AI Catalog</CardTitle>
-          <CardDescription>
-            Enter your email below to login to your account
-          </CardDescription>
+          <CardDescription>Log in using your google account.</CardDescription>
         </CardHeader>
-        <CardContent>
-          <form>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  placeholder="m@example.com"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
-                <Input id="password" type="password" required />
-              </div>
-            </div>
-          </form>
-        </CardContent>
+
         <CardFooter className="flex-col gap-2">
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
           <Button
             onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
             variant="outline"
