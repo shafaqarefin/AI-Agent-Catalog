5f0f68079c9f6db7ff3c8d730506e547803f9241
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-07 13:27:31 +0600

    all content wrapped aroiund session and redux store provider

---
+++ app/provider.tsx
@@ -0,0 +1,14 @@
+// app/provider.tsx
+"use client";
+
+import { SessionProvider } from "next-auth/react";
+import { Provider } from "react-redux";
+import { store } from "@/store";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <Provider store={store}>{children}</Provider>
+    </SessionProvider>
+  );
+}
