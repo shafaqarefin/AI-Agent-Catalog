f3c73813610a63af0ab4524feabe83a07e7d3d17
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-07 17:55:46 +0600

    Added loading animation to signout button

--- components/user/Signout.tsx
+++ components/user/Signout.tsx
@@ -1,9 +1,21 @@
+"use client";
+
+import React, { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { signOut } from "next-auth/react";
+import { Loader2 } from "lucide-react";
 
 export default function SignOutButton() {
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSignOut = async () => {
+    setIsLoading(true);
+    await signOut({ callbackUrl: "/" });
+  };
+
   return (
-    <Button variant="destructive" onClick={() => signOut({ callbackUrl: "/" })}>
+    <Button variant="destructive" onClick={handleSignOut} disabled={isLoading}>
+      {isLoading ? <Loader2 className="mr-2 h-4 w-4 animate-spin" /> : null}
       Sign Out
     </Button>
   );
