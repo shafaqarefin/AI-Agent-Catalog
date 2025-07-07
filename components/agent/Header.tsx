a7c2543ffeff37fcaba16239d1ee2b54ce7cb6b2
Shafaq Arefin Chowdhury <shafaqarefinchowdhury@gmail.com>
2025-07-07 17:17:21 +0600

    made header more responsive

--- components/agent/Header.tsx
+++ components/agent/Header.tsx
@@ -1,21 +1,22 @@
 import React from "react";
 import FiltersBar from "../filters/FiltersMenuBar";
 import Search from "../search/Search";
-import { Menubar } from "../ui/menubar";
 import { ClearFilterButton } from "../filters/ClearFilterIcon";
 
 function CatalogHeader() {
   return (
-    <Menubar className="bg-stone-800 text-white p-4 sm:p-6 flex flex-col sm:flex-row sm:items-center sm:justify-between gap-4 ">
-      <div className="flex flex-wrap items-center gap-3 sm:gap-4  ">
-        <FiltersBar />
-        <ClearFilterButton />
-      </div>
+    <header className="bg-stone-800 text-white p-4 sm:p-6 w-full">
+      <div className="flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
+        <div className="flex flex-col sm:flex-row sm:items-center sm:gap-4 sm:flex-nowrap w-full sm:w-auto min-w-0">
+          <FiltersBar />
+          <ClearFilterButton />
+        </div>
 
-      <div className="w-full sm:w-auto">
-        <Search />
+        <div className="w-full sm:w-auto min-w-0">
+          <Search />
+        </div>
       </div>
-    </Menubar>
+    </header>
   );
 }
 
